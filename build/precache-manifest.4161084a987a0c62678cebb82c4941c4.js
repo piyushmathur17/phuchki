5eebc2d4dbd628156156dbb88cbf7dbb8e6c3465
unknown <piyushmathur@gmail.com>
2020-07-02 01:07:25 +0530

    npm run build

---
+++ build/precache-manifest.4161084a987a0c62678cebb82c4941c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cb91bd15ab353329079",
+    "url": "/static/css/main.d1554a7e.chunk.css"
+  },
+  {
+    "revision": "4cb91bd15ab353329079",
+    "url": "/static/js/main.4cb91bd1.chunk.js"
+  },
+  {
+    "revision": "d9092bff31ba3540346b",
+    "url": "/static/js/1.d9092bff.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afa6a28d0ee0b5e7d55b7a5aecdfedec",
+    "url": "/static/media/empty-cart.afa6a28d.png"
+  },
+  {
+    "revision": "6aaff8de1279d81d27fbb877bafb0d36",
+    "url": "/index.html"
+  }
+];
